6042b20e47975b737c87201c0aa508744fa0153f
Govind Kailas <gkailas@fortinet.com>
2024-01-18 13:08:46 -0800

    simplfied it further

--- functions/knative/zen-quotes-go/quote.go
+++ functions/knative/zen-quotes-go/quote.go
@@ -1,61 +1,44 @@
-package quote
+package main
 
 import (
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
-
-	"github.com/OpenFunction/functions-framework-go/functions"
 )
 
-func init() {
-	functions.HTTP("quote", ZenQuote,
-		functions.WithFunctionPath("/quote"))
+func main() {
+	http.HandleFunc("/quote", quoteHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// respond back to the client with the quote author name and content.
-func respond(w http.ResponseWriter, quote string) {
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	w.Write([]byte(quote))
-}
+func quoteHandler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://api.quotable.io/random")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-func ZenQuote(w http.ResponseWriter, r *http.Request) {
-	//get the quote from https://api.quotable.io/quotes/random .
-	// this is the json response from the API [{"_id":"n4cqmf135I","author":"Alan Watts","content":"Things are as they are. Looking out into it the universe at night, we make no comparisons between right and wrong stars, nor between well and badly arranged constellations.","tags":["Philosophy","Self"],"authorSlug":"alan-watts","length":172,"dateAdded":"2022-03-12","dateModified":"2023-04-14"}]
-	// we need only author and content.
-	// we need to convert it to the following format
-	// {"author":"<NAME>","content":"Things are as they are. Looking out into it the universe at night, we make no comparisons between
-	// right and wrong stars, nor between well and badly arranged constellations."}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	var quote struct {
-		Author       string   `json:"author"`
-		Content      string   `json:"content"`
-		Tags         []string `json:"tags"`
-		AuthorSlug   string   `json:"authorSlug"`
-		Length       int      `json:"length"`
-		DateAdded    string   `json:"dateAdded"`
-		DateModified string   `json:"dateModified"`
+		Author  string `json:"author"`
+		Content string `json:"content"`
 	}
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	client := &http.Client{Transport: tr}
-
-	resp, err := client.Get("https://api.quotable.io/random")
+	err = json.Unmarshal(body, &quote)
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	defer resp.Body.Close()
-	err = json.NewDecoder(resp.Body).Decode(&quote)
-	if err != nil {
-		fmt.Println(err)
-	}
-	respond(w, quote.Author+" says: "+quote.Content)
 
+	fmt.Fprintf(w, "%s says: %s", quote.Author, quote.Content)
 }
